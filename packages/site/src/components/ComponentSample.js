9ef3db442b678c74e65cd8eae3df0f5b542a0e4a
Stefan Mansson <stefan.aotik@gmail.com>
2018-01-26 20:12:59 +0200

    feat: Show/hide snippets

--- packages/site/src/components/ComponentSample.js
+++ packages/site/src/components/ComponentSample.js
@@ -6,15 +6,25 @@ export default class ComponentSample extends Component {
     constructor() {
         super();
 
+        this.state = {
+            showingSnippet: false
+        };
+
         this.sampleSnippet = null;
+        this.toggleSnippet = this.toggleSnippet.bind(this);
     }
 
     componentDidMount() {
 
     }
 
+    toggleSnippet() {
+        this.setState({ showingSnippet: !this.state.showingSnippet });
+    }
+
     render() {
         const sample = this.props.sample;
+        const { showingSnippet } = this.state;
 
         return (
             <section className="component-sample" key={sample.title}>
@@ -22,7 +32,8 @@ export default class ComponentSample extends Component {
                     <h3>{sample.title}</h3>
                     <div dangerouslySetInnerHTML={{ __html: sample.descHtml }} />
                 </div>
-                <div className="module">
+                <div className={showingSnippet ? 'module open' : 'module'}>
+                    <span className="toggle" role="presentation" onClick={ this.toggleSnippet}>{ showingSnippet ? 'Hide snippet' : 'Show snippet' }</span>
                     <div className="content">
                         <div dangerouslySetInnerHTML={{ __html: sample.snippet }} />
                     </div>
