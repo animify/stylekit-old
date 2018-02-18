d3da12633219d6fbfe28cd128a6fe77f80d383cf
Stefan Mansson <stefan.aotik@gmail.com>
2018-02-18 11:12:05 +0200

    feat: Added label component

--- packages/site/src/definitions/typography.js
+++ packages/site/src/definitions/typography.js
@@ -5,5 +5,5 @@
 'fontSizeH4':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.325,"type":"em"}]},true),{case:undefined}),
 'fontSizeH5':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.125,"type":"em"}]},true),{case:undefined}),
 'fontSizeBody':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":15,"type":"px"}]},true),{case:undefined}),
-'fontSizeSub':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":13,"type":"px"}]},true),{case:undefined}),
-'fontSizeSmall':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":11,"type":"px"}]},true),{case:undefined})};})();
+'fontSizeSub':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":12,"type":"px"}]},true),{case:undefined}),
+'fontSizeSmall':new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":10,"type":"px"}]},true),{case:undefined})};})();
