00708c96963f40d6e84695c9f1569df61a27e6a1
Stefan Mansson <stefan.aotik@gmail.com>
2018-03-18 16:30:01 +0200

    refactor: Font size var refactor

--- packages/site/src/definitions/typography.js
+++ packages/site/src/definitions/typography.js
@@ -1,9 +1,2 @@
 (function(){var F=require('foovar/lib/FoovarValue');F.case=null;var S=require('foovar/lib/StylusExpression');var p=require('foovar/lib/convertToPlainObject');module.exports={
-'fontSizeH1':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":4.8,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeH2':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":3.6,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeH3':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":3,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeH4':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":2.4,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeH5':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.8,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeBody':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.6,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeSub':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.2,"type":"rem"}]},true),{case:undefined}),{from:'css'}),
-'fontSizeSmall':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Unit","val":1.1,"type":"rem"}]},true),{case:undefined}),{from:'css'})};})();
+'fontSize':p(new F(new S({"__type":"Expression","nodes":[{"__type":"Object","vals":{"h1":{"__type":"Expression","nodes":[{"__type":"Unit","val":4.8,"type":"rem"}]},"h2":{"__type":"Expression","nodes":[{"__type":"Unit","val":3.6,"type":"rem"}]},"h3":{"__type":"Expression","nodes":[{"__type":"Unit","val":3,"type":"rem"}]},"h4":{"__type":"Expression","nodes":[{"__type":"Unit","val":2.4,"type":"rem"}]},"h5":{"__type":"Expression","nodes":[{"__type":"Unit","val":1.8,"type":"rem"}]},"body":{"__type":"Expression","nodes":[{"__type":"Unit","val":1.6,"type":"rem"}]},"sub":{"__type":"Expression","nodes":[{"__type":"Unit","val":1.2,"type":"rem"}]},"small":{"__type":"Expression","nodes":[{"__type":"Unit","val":1.1,"type":"rem"}]}}}]},true),{case:undefined}),{from:'css'})};})();
