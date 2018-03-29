708bb6be6ae2e408868699d3a19e5719b1131ade
Stefan Mansson <stefan.aotik@gmail.com>
2018-03-29 21:12:24 +0300

    refactor: Page descriptions updated

--- packages/site/src/Router.js
+++ packages/site/src/Router.js
@@ -10,13 +10,13 @@ export default class Router extends Component {
                 <div>
                     <Route exact path="/" render={() => (<HomeLayout />)} />
 
-                    <Route exact path="/components/:type?" render={props => (<PageLayout {...props} guide="components" title="Components" description="Components do this and that this and that this and that this and that this and that this and that." />)} />
+                    <Route exact path="/components/:type?" render={props => (<PageLayout {...props} guide="components" title="Components" description="The atomic building blocks used throughout the design system to create collective containers." />)} />
 
-                    <Route exact path="/layout/:type?" render={props => (<PageLayout {...props} guide="layout" title="Layout" description="Components do this and that this and that this and that this and that this and that this and that." />)} />
+                    <Route exact path="/layout/:type?" render={props => (<PageLayout {...props} guide="layout" title="Layout" description="Structural units & helpers that make up the responsive & visual foundation of your page." />)} />
 
                     <Route exact path="/utility/:type?" render={props => (<PageLayout {...props} guide="utility" title="Utility" description="Components do this and that this and that this and that this and that this and that this and that." />)} />
 
-                    <Route exact path="/variables/:type?" render={props => (<PageLayout {...props} guide="variables" title="Variables" description="Components do this and that this and that this and that this and that this and that this and that." />)} />
+                    <Route exact path="/variables/:type?" render={props => (<PageLayout {...props} guide="variables" title="Variables" description="A collection of static style variables used universally by the stylekit for components and layout." />)} />
                 </div>
             </BrowserRouter>
         );
