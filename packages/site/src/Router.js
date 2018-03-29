af5fb800ce7e80217f153cd8e5430138cdde1a50
Stefan Mansson <stefan.aotik@gmail.com>
2018-03-29 21:38:13 +0300

    refactor: Description update for Utility

--- packages/site/src/Router.js
+++ packages/site/src/Router.js
@@ -14,7 +14,7 @@ export default class Router extends Component {
 
                     <Route exact path="/layout/:type?" render={props => (<PageLayout {...props} guide="layout" title="Layout" description="Structural units & helpers that make up the responsive & visual foundation of your page." />)} />
 
-                    <Route exact path="/utility/:type?" render={props => (<PageLayout {...props} guide="utility" title="Utility" description="Components do this and that this and that this and that this and that this and that this and that." />)} />
+                    <Route exact path="/utility/:type?" render={props => (<PageLayout {...props} guide="utility" title="Utility" description="Useful classes & methods that assist in managing elements within a structured layout." />)} />
 
                     <Route exact path="/variables/:type?" render={props => (<PageLayout {...props} guide="variables" title="Variables" description="A collection of static style variables used universally by the stylekit for components and layout." />)} />
                 </div>
