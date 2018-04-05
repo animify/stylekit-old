01118e63fd0f7753cd3e17e2d391d0a2c79dca9c
Stefan Mansson <stefan.aotik@gmail.com>
2018-04-05 20:24:21 +0300

    refactor: Router pages component loop

--- packages/site/src/Router.js
+++ packages/site/src/Router.js
@@ -4,19 +4,49 @@ import HomeLayout from './containers/HomeLayout';
 import PageLayout from './containers/PageLayout';
 
 export default class Router extends Component {
+    static get stylePages() {
+        return [
+            {
+                title: 'Components',
+                description: 'The atomic building blocks used throughout the design system to create collective containers.',
+                path: '/components/:type?',
+                guide: 'components',
+                active: true
+            },
+            {
+                title: 'Layout',
+                description: 'Structural units & helpers that make up the responsive & visual foundation of your page.',
+                path: '/layout/:type?',
+                guide: 'layout',
+                active: true
+            },
+            {
+                title: 'Utility',
+                description: 'Useful classes & methods that assist in managing elements within a structured layout.',
+                path: '/utility/:type?',
+                guide: 'utility',
+                active: true
+            },
+            {
+                title: 'Variables',
+                description: 'A collection of static style variables used universally by the stylekit for components and layout.',
+                path: '/variables/:type?',
+                guide: 'variables',
+                active: true
+            }
+        ];
+    }
+
     render() {
         return (
             <BrowserRouter>
                 <div>
                     <Route exact path="/" render={() => (<HomeLayout />)} />
 
-                    <Route exact path="/components/:type?" render={props => (<PageLayout {...props} guide="components" title="Components" description="The atomic building blocks used throughout the design system to create collective containers." />)} />
+                    { Router.stylePages.map(page => (
+                        <Route exact path={page.path} render={props => (<PageLayout {...props} guide={page.guide} title={page.title} description={page.description} />)} />
+                    )) }
 
-                    <Route exact path="/layout/:type?" render={props => (<PageLayout {...props} guide="layout" title="Layout" description="Structural units & helpers that make up the responsive & visual foundation of your page." />)} />
-
-                    <Route exact path="/utility/:type?" render={props => (<PageLayout {...props} guide="utility" title="Utility" description="Useful classes & methods that assist in managing elements within a structured layout." />)} />
-
-                    <Route exact path="/variables/:type?" render={props => (<PageLayout {...props} guide="variables" title="Variables" description="A collection of static style variables used universally by the stylekit for components and layout." />)} />
                 </div>
             </BrowserRouter>
         );
