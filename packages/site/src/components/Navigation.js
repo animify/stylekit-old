e79051b26c51ad0d0b00d3a5b05737d7761c249c
Stefan Mansson <st.mansson@icloud.com>
2018-04-29 13:13:02 +0300

    fix: Navigation active fix

--- packages/site/src/components/Navigation.js
+++ packages/site/src/components/Navigation.js
@@ -5,30 +5,12 @@ import logo from './../public/images/logotype.svg';
 import Constants from './../utils/Constants';
 
 export default class Navigation extends Component {
-    state = {
-        currentPage: undefined,
-    }
-
     changedPage = () => {
         window.scrollTo(0, 0);
     }
 
-    isHome = () => location.pathname === '/';
-
-    setNav = (currentPage) => {
-        this.setState({ currentPage });
-    }
-
-    setHome = () => {
-        window.scrollTo(0, 0);
-
-        this.setState({
-            currentPage: undefined,
-        });
-    }
-
     render() {
-        const { sections, currentPage } = this.props;
+        const { guide } = this.props;
 
         return (
             <header className="nav menu main header hasborder">
@@ -36,10 +18,10 @@ export default class Navigation extends Component {
                     <div className="set">
                         <Link to="/" onClick={this.setHome} className="item logo"><img src={logo} height="24" alt="Stylekit primary logo" /></Link>
                         <div className="float-right">
-                            <Link to="/components" onClick={this.changedPage} className={!this.isHome() && currentPage === 'components' ? 'item active' : 'item'}>Components</Link>
-                            <Link to="/layout" onClick={this.changedPage} className={!this.isHome() && currentPage === 'layout' ? 'item active' : 'item'}>Layout</Link>
-                            <Link to="/utility" onClick={this.changedPage} className={!this.isHome() && currentPage === 'utility' ? 'item active' : 'item'}>Utility</Link>
-                            <Link to="/variables" onClick={this.changedPage} className={!this.isHome() && currentPage === 'variables' ? 'item active' : 'item'}>Variables</Link>
+                            <Link to="/components" onClick={this.changedPage} className={guide === 'components' ? 'item active' : 'item'}>Components</Link>
+                            <Link to="/layout" onClick={this.changedPage} className={guide === 'layout' ? 'item active' : 'item'}>Layout</Link>
+                            <Link to="/utility" onClick={this.changedPage} className={guide === 'utility' ? 'item active' : 'item'}>Utility</Link>
+                            <Link to="/variables" onClick={this.changedPage} className={guide === 'variables' ? 'item active' : 'item'}>Variables</Link>
                         </div>
                     </div>
                 </div>
