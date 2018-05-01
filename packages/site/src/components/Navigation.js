9a9d574403d6d0238613f1c3006572b265021947
Stefan Mansson <st.mansson@icloud.com>
2018-05-01 20:57:36 +0300

    refactor: Font weight update for menu & list

--- packages/site/src/components/Navigation.js
+++ packages/site/src/components/Navigation.js
@@ -15,14 +15,12 @@ export default class Navigation extends Component {
         return (
             <header className="nav menu main header hasborder">
                 <div className="container">
-                    <div className="set">
-                        <Link to="/" onClick={this.setHome} className="item logo"><img src={logo} height="24" alt="Stylekit primary logo" /></Link>
-                        <div className="float-right">
-                            <Link to="/components" onClick={this.changedPage} className={guide === 'components' ? 'item active' : 'item'}>Components</Link>
-                            <Link to="/layout" onClick={this.changedPage} className={guide === 'layout' ? 'item active' : 'item'}>Layout</Link>
-                            <Link to="/utility" onClick={this.changedPage} className={guide === 'utility' ? 'item active' : 'item'}>Utility</Link>
-                            <Link to="/variables" onClick={this.changedPage} className={guide === 'variables' ? 'item active' : 'item'}>Variables</Link>
-                        </div>
+                    <Link to="/" onClick={this.setHome} className="item logo"><img src={logo} height="20" alt="Stylekit primary logo" /></Link>
+                    <div className="float-right">
+                        <Link to="/components" onClick={this.changedPage} className={guide === 'components' ? 'item active' : 'item'}>Components</Link>
+                        <Link to="/layout" onClick={this.changedPage} className={guide === 'layout' ? 'item active' : 'item'}>Layout</Link>
+                        <Link to="/utility" onClick={this.changedPage} className={guide === 'utility' ? 'item active' : 'item'}>Utility</Link>
+                        <Link to="/variables" onClick={this.changedPage} className={guide === 'variables' ? 'item active' : 'item'}>Variables</Link>
                     </div>
                 </div>
             </header>
